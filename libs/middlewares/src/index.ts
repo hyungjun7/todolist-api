f66443427960ba9b065b83efdb54bac518d5f765
ISAAC LEE <adsdfsd2321@naver.com>
2023-01-23 05:25:48 +0900

    feat: use assign id middleware

--- libs/middlewares/src/index.ts
+++ libs/middlewares/src/index.ts
@@ -1,2 +1,1 @@
-export * from './middlewares.module';
-export * from './middlewares.service';
+export * from './assign-id.middleware'
--- libs/middlewares/src/middlewares.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MiddlewaresService } from './middlewares.service';
-
-@Module({
-  providers: [MiddlewaresService],
-  exports: [MiddlewaresService],
-})
-export class MiddlewaresModule {}
