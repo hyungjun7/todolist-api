f7d63b8f209b15a4e0005f3c6a8f69b7552be319
isaac lee <adsdfsd2321@naver.com>
2023-01-23 20:09:59 +0900

    feat: global exception handler (#4)
    * feat: use base exception class
    
    * feat: add global error handler

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,15 +1,24 @@
 import { CoreModule } from '@app/core'
+import { ExceptionHandler } from '@app/core/common/exception.handler'
 import { LoggerModule } from '@app/logger'
 import { AssignIdMiddleware } from '@app/middlewares'
 import { LoggingMiddleware } from '@app/middlewares/logging.middleware'
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { APP_FILTER } from '@nestjs/core'
 import { ApiController } from './api.controller'
 import { ApiService } from './api.service'
 
 @Module({
   imports: [CoreModule, LoggerModule],
   controllers: [ApiController],
-  providers: [ApiService, LoggingMiddleware]
+  providers: [
+    ApiService,
+    LoggingMiddleware,
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionHandler
+    }
+  ]
 })
 export class ApiModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
