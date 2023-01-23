6ed8f22aec9602c764c8bcb4cf2c51eb0849331f
isaac lee <adsdfsd2321@naver.com>
2023-01-23 17:22:41 +0900

    Feat/logging (#3)
    * refactor: logger method refactoring
    
    * feat: logging middleware

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,13 +1,15 @@
 import { CoreModule } from '@app/core'
+import { LoggerModule } from '@app/logger'
 import { AssignIdMiddleware } from '@app/middlewares'
+import { LoggingMiddleware } from '@app/middlewares/logging.middleware'
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { ApiController } from './api.controller'
 import { ApiService } from './api.service'
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, LoggerModule],
   controllers: [ApiController],
-  providers: [ApiService]
+  providers: [ApiService, LoggingMiddleware]
 })
 export class ApiModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,14 @@
+import { LoggerService } from '@app/logger'
+import { LoggingMiddleware } from '@app/middlewares/logging.middleware'
 import { NestFactory } from '@nestjs/core'
 import { ApiModule } from './api.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule)
+  const logger = app.get(LoggerService)
+  const loggingMiddleware = app.get(LoggingMiddleware)
+  app.useLogger(logger)
+  app.use(loggingMiddleware.module)
   await app.listen(4000)
 }
 bootstrap()
