673ddd96ed91bd0d021005442663b15d8f234842
ISAAC LEE <adsdfsd2321@naver.com>
2023-01-23 20:08:42 +0900

    feat: add global error handler

---
+++ libs/core/src/common/exception.handler.ts
@@ -0,0 +1,21 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus
+} from '@nestjs/common'
+import { LoggerService } from '@app/logger/logger.service'
+
+@Catch()
+export class ExceptionHandler implements ExceptionFilter {
+  constructor(private logger: LoggerService) {}
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx: any = host.switchToHttp()
+    const request = ctx.args[2].req
+    const body = { reqId: request.id }
+    this.logger.error(exception, body)
+    throw exception
+  }
+}
